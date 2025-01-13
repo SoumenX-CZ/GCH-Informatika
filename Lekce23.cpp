08f0280eb02c8f3c2ff0bc084be4432a9ca1c793
SoumenX <d.simon@outlook.cz>
2025-01-13 15:45:03 +0100

    Selection sort

---
+++ Lekce23.cpp
@@ -0,0 +1,29 @@
+//Lekce 23 - Selection sort
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[8] = {64, 25, 12, 22, 11, 90, 34, 50};
+    int n = 8;
+
+    for (int i = 0; i < n-1; i++) {
+        int min_idx = i;
+        for (int j = i+1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+        // Swap the found minimum element with the first element
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+
+    cout << "Sorted array: \n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
